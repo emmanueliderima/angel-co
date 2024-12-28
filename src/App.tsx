6364cf87b8ddb45ccd1324e43c5da45caafa7c5e
Emmanuel <iderimaemmanuel24@gmail.com>
2024-12-28 13:37:58 +0100

    fix: postcss config errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { Button } from "./components/ui/button"
 
 function App() {
 
   return (
     <>
-     <h1>Hi</h1>
+    <div>
+      <Button title="Click" className="bg-success-600">Click</Button>
+      <button className="bg-success-700 text-success-600 p-3">Button</button>
+    </div>
+     <h1 className="text-semibold border border-solid border-green-300">Hi</h1>
     </>
   )
 }
