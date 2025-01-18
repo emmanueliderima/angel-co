3fbdb88411d9e0f092e3a57ae9e4eff5fa5b28b5
Emmanuel <iderimaemmanuel24@gmail.com>
2025-01-18 18:41:26 +0100

    chore: codebase cleanup

--- src/pages/Previous.tsx
+++ src/pages/Previous.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Previous = () => {
   return (
-    <section className="border border-solid border-purple-1 flex size-full flex-col text-white">
+    <section className="bg-dark-2 flex size-full flex-col text-white">
       <h1 className="text-3xl font-bold">
         Previous
       </h1>
